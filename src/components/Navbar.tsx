95d2472b114f030fef096047f91b8a5f9032f461
Sangjin Kim <rla2892@naver.com>
2024-09-16 12:16:22 +0900

    Style for navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,22 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
+    const linkClass = `text-white font-bold hover:underline`
+
     return (
-        <nav>
-            <ul>
+        <nav className="bg-gray-800 p-4">
+            <ul className="flex justify-around items-center">
                 <li>
-                    <Link to="/">Dashboard</Link>
+                    <Link to="/" className={linkClass}>Dashboard</Link>
                 </li>
                 <li>
-                    <Link to="/transactions">Transactions</Link>
+                    <Link to="/transactions" className={linkClass}>Transactions</Link>
                 </li>
                 <li>
-                    <Link to="/budget">Budget</Link>
+                    <Link to="/budget" className={linkClass}>Budget</Link>
                 </li>
                 <li>
-                    <Link to="/categories">Categories</Link>
+                    <Link to="/categories" className={linkClass}>Categories</Link>
                 </li>
             </ul>
         </nav>
